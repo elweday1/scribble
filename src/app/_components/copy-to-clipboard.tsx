bf98f6f7732f952d3946945a727f2b69c4edec2e
elweday1 <mohammednh2864@gmail.com>
2024-03-21 15:39:11 +0200

    fix copy button

--- src/app/_components/copy-to-clipboard.tsx
+++ src/app/_components/copy-to-clipboard.tsx
@@ -11,20 +11,22 @@ export default function ({ copyText }: { copyText: string }) {
     }
     
     return (
-        <button onClick={copy} className="w-full place-content-center border-2 px-4 py-4 rounded-lg bg-black/10 hover:bg-black/20 pl-1 gap-1 hover:scale-[1.02] transition-all col-span-2 flex place-items-center ">
+        <button onClick={copy} className="flex place-content-center w-full border-2 px-2 py-2 sm:px-4 sm:py-4 rounded-lg bg-black/20 hover:bg-black/30 hover:scale-[1.02] transition-all col-span-2 place-items-center ">
         {!isCopied ?
-          <svg className="size-9" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-	<g fill="none" stroke="currentColor">
-		<rect width="9" height="13" x="6.5" y="6.5" rx="1.5" />
-		<path d="M8.5 6A1.5 1.5 0 0 1 10 4.5h6A1.5 1.5 0 0 1 17.5 6v10a1.5 1.5 0 0 1-1.5 1.5" />
-	</g>
-</svg>      :
-<svg className="size-9" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24">
-	<path fill="currentColor" d="m10 15.586l-3.293-3.293l-1.414 1.414L10 18.414l9.707-9.707l-1.414-1.414z" />
-</svg>        }
-{!isCopied ?
- copyText
- : "Copied!"}
+          (
+            <>
+              <svg className="size-9" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M15 1.25h-4.056c-1.838 0-3.294 0-4.433.153c-1.172.158-2.121.49-2.87 1.238c-.748.749-1.08 1.698-1.238 2.87c-.153 1.14-.153 2.595-.153 4.433V16a3.751 3.751 0 0 0 3.166 3.705c.137.764.402 1.416.932 1.947c.602.602 1.36.86 2.26.982c.867.116 1.97.116 3.337.116h3.11c1.367 0 2.47 0 3.337-.116c.9-.122 1.658-.38 2.26-.982c.602-.602.86-1.36.982-2.26c.116-.867.116-1.97.116-3.337v-5.11c0-1.367 0-2.47-.116-3.337c-.122-.9-.38-1.658-.982-2.26c-.531-.53-1.183-.795-1.947-.932A3.751 3.751 0 0 0 15 1.25m2.13 3.021A2.25 2.25 0 0 0 15 2.75h-4c-1.907 0-3.261.002-4.29.14c-1.005.135-1.585.389-2.008.812c-.423.423-.677 1.003-.812 2.009c-.138 1.028-.14 2.382-.14 4.289v6a2.25 2.25 0 0 0 1.521 2.13c-.021-.61-.021-1.3-.021-2.075v-5.11c0-1.367 0-2.47.117-3.337c.12-.9.38-1.658.981-2.26c.602-.602 1.36-.86 2.26-.981c.867-.117 1.97-.117 3.337-.117h3.11c.775 0 1.464 0 2.074.021M7.408 6.41c.277-.277.665-.457 1.4-.556c.754-.101 1.756-.103 3.191-.103h3c1.435 0 2.436.002 3.192.103c.734.099 1.122.28 1.399.556c.277.277.457.665.556 1.4c.101.754.103 1.756.103 3.191v5c0 1.435-.002 2.436-.103 3.192c-.099.734-.28 1.122-.556 1.399c-.277.277-.665.457-1.4.556c-.755.101-1.756.103-3.191.103h-3c-1.435 0-2.437-.002-3.192-.103c-.734-.099-1.122-.28-1.399-.556c-.277-.277-.457-.665-.556-1.4c-.101-.755-.103-1.756-.103-3.191v-5c0-1.435.002-2.437.103-3.192c.099-.734.28-1.122.556-1.399" clipRule="evenodd"></path></svg>
+              <span className="scale-[0.8] origin-center md:scale-100 truncate">{copyText}</span>
+            </>
+          )
+          :
+          (
+          <>
+            <svg className="size-9" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="currentColor" d="m10 15.586l-3.293-3.293l-1.414 1.414L10 18.414l9.707-9.707l-1.414-1.414z" /></svg> 
+            <span className="scale-[0.8] origin-center md:scale-100">Copied</span>
+          </>
+          )       
+        }
       </button>
 
     );
